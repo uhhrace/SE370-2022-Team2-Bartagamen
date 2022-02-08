e0bd8f797deb0de1bfee6a540d1792855a19a335
rbays <RBays@tandemdiabetes.com>
2022-02-07 17:06:13 -0800

    Updating Lab2 source code

--- Lab2/TicTacToe.java
+++ Lab2/TicTacToe.java
@@ -42,7 +42,6 @@ public class TicTacToe       //declares class
 
 	static int[] getMove(boolean isX){
 
-
 		int xCoord;
 		int yCoord;
 
@@ -96,46 +95,6 @@ public class TicTacToe       //declares class
 		// For each row, check XXX or OOO
 		for(int j = 0; j <= 2; j++){
 			for(int k = 0; k <= 2; k++){
-				position[0] = j;
-				position[1] = k;
-
-				positionChar = getBoard(position);
-
-				if(positionChar == SYMBOL_X){
-					score++;
-				}else if(positionChar == SYMBOL_O){
-					score--;
-				}else{
-					//Do nothing
-				}
-
-				if(3 == score){
-					//X wins
-					System.out.println("Player 1 wins!");
-					gameState = GAME_STATUS.WIN;
-					//Print the final board state
-					printBoard();
-					// Early return
-					return gameState;
-				}else if(-3 == score){
-					//O wins
-					System.out.println("Player 2 wins!");
-					gameState = GAME_STATUS.WIN;
-					//Print the final board state
-					printBoard();
-					// Early return
-					return gameState;
-				}else{
-					//nothing
-				}
-			}
-			// Reset score to count next row
-			score = 0;
-		}
-
-		// For each column, check XXX or OOO
-		for(int j = 0; j <= 2; j++){
-			for(int k = 0; k <= 2; k++){
 				position[1] = j;
 				position[0] = k;
 
@@ -171,8 +130,51 @@ public class TicTacToe       //declares class
 			}
 			// Reset score to count next row
 			score = 0;
+			position[1] = 0;
+			position[0] = 0;
 		}
 
+		// For each column, check XXX or OOO
+		for(int j = 0; j <= 2; j++){
+			for(int k = 0; k <= 2; k++){
+				position[0] = j;
+				position[1] = k;
+
+				positionChar = getBoard(position);
+
+				if(positionChar == SYMBOL_X){
+					score++;
+				}else if(positionChar == SYMBOL_O){
+					score--;
+				}else{
+					//Do nothing
+				}
+
+				if(3 == score){
+					//X wins
+					System.out.println("Player 1 wins!");
+					gameState = GAME_STATUS.WIN;
+					//Print the final board state
+					printBoard();
+					// Early return
+					return gameState;
+				}else if(-3 == score){
+					//O wins
+					System.out.println("Player 2 wins!");
+					gameState = GAME_STATUS.WIN;
+					//Print the final board state
+					printBoard();
+					// Early return
+					return gameState;
+				}else{
+					//nothing
+				}
+			}
+			// Reset score to count next row
+			score = 0;
+			position[1] = 0;
+			position[0] = 0;
+		}
 
 		// Check diagonals for XXX or OOO
 		position[0] = 0;
@@ -226,6 +228,7 @@ public class TicTacToe       //declares class
 		// Check diagonals for XXX or OOO
 		position[0] = 2;
 		position[1] = 0;
+		score = 0;
 
 		positionChar = getBoard(position);
 		if(SYMBOL_X == positionChar){
@@ -271,9 +274,6 @@ public class TicTacToe       //declares class
 			// Early return
 			return gameState;
 		}
-
-		//Print the final board state
-		//printBoard();
 		
 		return gameState;
 	}
@@ -300,24 +300,23 @@ public class TicTacToe       //declares class
 		char[][] tempBoard = new char[3][3];
 		int[] cords = new int[2];
 		
-		for(int j = 0; j <= 2; j++){
-			for(int k = 0; k <= 2; k++){
-				// Very inefficient, figure out how to simplify declaring array here
-				cords[0] = j;
-				cords[1] = k;
-				tempBoard[j][k] = getBoard(cords);
+		for(int y = 0; y <= 2; y++){
+			for(int x = 0; x <= 2; x++){
+				cords[0] = x;
+				cords[1] = y;
+				tempBoard[x][y] = getBoard(cords);
 			}
 		}
 
 		System.out.printf(" _______________________\n");
 		System.out.printf("|       |       |       |\n");
-		System.out.printf("|   %c   |   %c   |   %c   |\n", getBoard(new int[]{0,0}), getBoard(new int[]{0,1}), getBoard(new int[]{0,2}));
+		System.out.printf("|   %c   |   %c   |   %c   |\n", getBoard(new int[]{0,0}), getBoard(new int[]{1,0}), getBoard(new int[]{2,0}));
 		System.out.printf("|_______|_______|_______|\n");
 		System.out.printf("|       |       |       |\n");
-		System.out.printf("|   %c   |   %c   |   %c   |\n", getBoard(new int[]{1,0}), getBoard(new int[]{1,1}), getBoard(new int[]{1,2}));
+		System.out.printf("|   %c   |   %c   |   %c   |\n", getBoard(new int[]{0,1}), getBoard(new int[]{1,1}), getBoard(new int[]{2,1}));
 		System.out.printf("|_______|_______|_______|\n");
 		System.out.printf("|       |       |       |\n");
-		System.out.printf("|   %c   |   %c   |   %c   |\n", getBoard(new int[]{2,0}), getBoard(new int[]{2,1}), getBoard(new int[]{2,2}));
+		System.out.printf("|   %c   |   %c   |   %c   |\n", getBoard(new int[]{0,2}), getBoard(new int[]{1,2}), getBoard(new int[]{2,2}));
 		System.out.printf("|_______|_______|_______|\n");
 	}
 
