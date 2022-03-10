2896f8b2645c63b94cc1d891c34f9a2cd86f9e19
janikwa41 <Sd21!_#ae2>
2022-03-10 14:13:05 -0800

    Navbar

--- Android App/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android App/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.item1:
+                    selectedFragment = new Home();
+                    break;
+                case R.id.item2:
+                    selectedFragment = new Calender();
+                    break;
+                case R.id.item3:
+                    selectedFragment = new Pets();
+                    break;
+                case R.id.item4:
+                    selectedFragment = new Food();
+                    break;
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, selectedFragment).commit();
+            return true;
+        }
+    };
+
 }
