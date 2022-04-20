de4f7647c5c2801afba67438395a65a0a760ad04
janikwa41 <Janik.Wolf@all-for-one.com>
2022-04-19 18:10:43 -0700

    Modification Toogle Buttons Food-Items

--- Android App/app/src/main/java/com/example/myapplication/Food.java
+++ Android App/app/src/main/java/com/example/myapplication/Food.java
@@ -1,19 +1,33 @@
 package com.example.myapplication;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Food extends BartScreen {
+
+
+    Context context;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.food, container, false);
+        View view = inflater.inflate(R.layout.food, container, false);
 
+
+
+        return view;
     }
 }
+
+
